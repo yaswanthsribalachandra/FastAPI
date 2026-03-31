0761fad7585d5abdc60f724dbf61428f23e2b7eb
DASARI YASWANTH SRI BALACHANDRA <dasariyaswanthsribalachandra@DASARIs-MacBook-Air.local>
2026-03-31 18:29:36 +0530

    Updated schema

--- schema.py
+++ schema.py
@@ -1,4 +1,7 @@
 from pydantic import BaseModel
+from beanie import Document
+from datetime import datetime
+from pydantic import Field
 
 class Employee(BaseModel):
     id: int
@@ -7,3 +10,10 @@ class Employee(BaseModel):
     position: str
     salary : int
 
+
+class Task(Document):
+    title: str
+    is_completed: bool = False
+
+    class Settings:
+        name = "task"
