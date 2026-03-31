b8abc7ec925a41e48b380a987162645add9151c8
DASARI YASWANTH SRI BALACHANDRA <dasariyaswanthsribalachandra@DASARIs-MacBook-Air.local>
2026-03-31 18:46:56 +0530

    updated with new routes

--- main.py
+++ main.py
@@ -4,13 +4,28 @@ from employees import employeerouter
 from books import booksrouter
 from health import healthrouter
 from crud import task_router
+from crud import task_router
+from contextlib import asynccontextmanager
+from database import init_db
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    await init_db()
+    print("Database connected ✅")
+    
+    yield
+    
+    # shutdown (optional)
+    print("App shutting down 🔴")
 
 app = FastAPI(
+    lifespan=lifespan,
     title="Employee API",
     description="API for managing a collection of employees",
     version="1.0.0"
 )
+app.include_router(task_router)
 
 app.include_router(employeerouter)
 app.include_router(booksrouter)
